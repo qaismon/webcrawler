4009615e56a9bd720f11b816ee360067b85886bb
qaismon <qaismon127@gmail.com>
2026-05-05 16:07:51 +0530

    feat: retry mechanism. "fetchWithretry( )" - No more “infinite crawling” -No duplicate spam URL normalization removes: ?tab=... #fragment  -Stable concurrency -Predictable termination

--- main.go
+++ main.go
@@ -20,15 +20,21 @@ var (
 	count int
 	mu sync.Mutex
 	wg sync.WaitGroup
-	baseHost = "github.com/qaismon"
+	baseHost = "github.com"
 )
 
-const maxDepth=2
+const (
+	maxDepth=2
+	maxPages=100
+)
+
 
 
 func main() {
-	jobs:=make(chan Job, 100)
-	rate:= time.Tick(300 * time.Millisecond)
+	jobs:=make(chan Job, 20)
+	ticker:= time.NewTicker(300 * time.Millisecond)
+	defer ticker.Stop()
+	rate:= ticker.C
 	
 	for i:=0;i<5;i++{
 		go worker(jobs, rate)
@@ -55,7 +61,7 @@ func main() {
 func worker(jobs chan Job, rate <-chan time.Time){
 	for job:=range jobs{
 
-		if job.Depth>maxDepth{
+		if job.Depth>=maxDepth{
 			wg.Done()
 			continue
 		}
@@ -64,8 +70,9 @@ func worker(jobs chan Job, rate <-chan time.Time){
 
 		fmt.Println("Crawling:", job.URL, "Depth:", job.Depth)
 
-		resp, err:= http.Get(job.URL)
-		if(err!=nil){
+		resp, err:= fetchWithRetry(job.URL, 3)
+		if err != nil {
+			fmt.Println("Failed:", job.URL)
 			wg.Done()
 			continue
 		}
@@ -84,9 +91,30 @@ func worker(jobs chan Job, rate <-chan time.Time){
 }
 
 
+func fetchWithRetry(url string, maxRetries int) (*http.Response, error) {
+	var resp *http.Response
+	var err error
+
+	for i:=0;i<maxRetries;i++{
+
+		resp, err = http.Get(url)
+		if err == nil{
+			return resp, nil
+		}
+
+		wait:= time.Duration(200*(1<<i)) * time.Millisecond
+		fmt.Println("Retrying:", url, "in", wait)
+		time.Sleep(wait)
+	}
+
+	return nil, err
+}
+
+
 func extractLinks(n *html.Node, jobs chan Job, depth int) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, attr := range n.Attr {
+
 			if attr.Key == "href" {
 				link:=attr.Val
 
@@ -95,6 +123,9 @@ func extractLinks(n *html.Node, jobs chan Job, depth int) {
 					continue
 				}
 
+				u.Fragment=""
+				u.RawQuery=""
+
 				if u.Host != "" && u.Host!=baseHost{
 					continue
 				}
@@ -102,10 +133,17 @@ func extractLinks(n *html.Node, jobs chan Job, depth int) {
 				if u.Host==""{
 					u.Scheme="https"
 					u.Host= baseHost
-					link=u.String()
+					
 				}
 
+				link=u.String()
+
 					mu.Lock()
+
+					if count >= maxPages {
+					mu.Unlock()
+					return
+				}
 
 					if !visited[link]{
 						visited[link]=true
